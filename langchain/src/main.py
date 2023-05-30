99f3b9cc30fad757b6d84aff0bbd8662f7343216
ryouhama <hary0730cap@icloud.com>
2023-05-31 00:57:01 +0900

    chat形式に対応

--- langchain/src/main.py
+++ langchain/src/main.py
@@ -1,21 +1,37 @@
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain import PromptTemplate, LLMChain
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
 
 
 def run():
     llm = ChatOpenAI(temperature=0)
+    memory = ConversationBufferMemory(return_messages=True)
 
-    template = """Question: {question}
-    Answer: """
+    template = "以下は、人間とAIの友好的な会話です。AIは人間の発言を理解し、それに対して適切な返答をします。"
 
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    # テンプレートを用いてプロンプトを作成
+    prompt = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(template),
+        MessagesPlaceholder(variable_name="history"),
+        HumanMessagePromptTemplate.from_template("{input}")
+    ])
+    conversation = ConversationChain(llm=llm, memory=memory, prompt=prompt)
 
-    question = "あなたの名前はなんですか？"
-    answer = llm_chain.run(question)
+    command = input("😇 : ")
 
-    print(answer)
+    while True:
+        response = conversation.predict(input=command)
+        print(f"🤖 : {response}")
+        command = input("😇 : ")
+        if command == "exit":
+            break
 
 
 if __name__ == "__main__":
